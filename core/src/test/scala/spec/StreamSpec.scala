25d6026d5c02b8c45f83b84e45c7f0f7efbbc591
Matt Hicks <matt@outr.com>
2024-12-06 07:16:16 -0600

    Fixed tests

--- core/src/test/scala/spec/StreamSpec.scala
+++ core/src/test/scala/spec/StreamSpec.scala
@@ -7,27 +7,27 @@ import rapid.{Stream, Task}
 class StreamSpec extends AnyWordSpec with Matchers {
   "Stream" should {
     "correctly map elements" in {
-      val stream = Stream.fromList(List(1, 2, 3, 4))
+      val stream = Stream.emits(List(1, 2, 3, 4))
       val result = stream.map(_ * 2).toList.sync()
       result shouldEqual List(2, 4, 6, 8)
     }
     "correctly flatMap elements" in {
-      val stream = Stream.fromList(List(1, 2, 3))
-      val result = stream.flatMap(x => Stream.fromList(List(x, x * 2))).toList.sync()
+      val stream = Stream.emits(List(1, 2, 3))
+      val result = stream.flatMap(x => Stream.emits(List(x, x * 2))).toList.sync()
       result shouldEqual List(1, 2, 2, 4, 3, 6)
     }
     "filter elements based on a predicate" in {
-      val stream = Stream.fromList(List(1, 2, 3, 4, 5))
+      val stream = Stream.emits(List(1, 2, 3, 4, 5))
       val result = stream.filter(_ % 2 == 0).toList.sync()
       result shouldEqual List(2, 4)
     }
     "take elements while a condition holds" in {
-      val stream = Stream.fromList(List(1, 2, 3, 4, 5))
+      val stream = Stream.emits(List(1, 2, 3, 4, 5))
       val result = stream.takeWhile(_ < 4).toList.sync()
       result shouldEqual List(1, 2, 3)
     }
     "evaluate elements sequentially with evalMap" in {
-      val stream = Stream.fromList(List(1, 2, 3))
+      val stream = Stream.emits(List(1, 2, 3))
       val result = stream.evalMap(x => Task(x * 2)).toList.sync()
       result shouldEqual List(2, 4, 6)
     }
@@ -42,8 +42,8 @@ class StreamSpec extends AnyWordSpec with Matchers {
       result.sorted shouldEqual List(2, 4, 6, 8, 10, 12) // Sorting to account for parallel execution order
     }*/
     "append two streams" in {
-      val stream1 = Stream.fromList(List(1, 2, 3))
-      val stream2 = Stream.fromList(List(4, 5, 6))
+      val stream1 = Stream.emits(List(1, 2, 3))
+      val stream2 = Stream.emits(List(4, 5, 6))
       val result = stream1.append(stream2).toList.sync()
       result shouldEqual List(1, 2, 3, 4, 5, 6)
     }
@@ -53,7 +53,7 @@ class StreamSpec extends AnyWordSpec with Matchers {
       result shouldEqual List.empty[Int]
     }
     "convert a stream to a list" in {
-      val stream = Stream.fromList(List(1, 2, 3, 4, 5))
+      val stream = Stream.emits(List(1, 2, 3, 4, 5))
       val result = stream.toList.sync()
       result shouldEqual List(1, 2, 3, 4, 5)
     }
