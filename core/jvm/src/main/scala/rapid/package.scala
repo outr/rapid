5d0dee025d3c3657b56b3eba8ece88fdf4780b2f
Matt Hicks <matt@outr.com>
2024-12-15 07:50:58 -0600

    Release 0.2.2
    Fixes for Blockable conversion

--- core/jvm/src/main/scala/rapid/package.scala
+++ core/jvm/src/main/scala/rapid/package.scala
@@ -1,6 +1,19 @@
+import scala.concurrent.duration.FiniteDuration
 import scala.language.implicitConversions
 
 package object rapid extends RapidPackage {
   implicit def fiber2Blockable[Return](fiber: Fiber[Return]): Blockable[Return] =
     fiber.asInstanceOf[Blockable[Return]]
+
+  implicit class BlockableTask[Return](task: Task[Return]) extends Blockable[Return] {
+    override def await(): Return = task
+      .start()
+      .asInstanceOf[Blockable[Return]]
+      .await()
+
+    override def await(duration: FiniteDuration): Option[Return] = task
+      .start()
+      .asInstanceOf[Blockable[Return]]
+      .await(duration)
+  }
 }
