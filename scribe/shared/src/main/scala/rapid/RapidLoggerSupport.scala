3eb32e4624922e7b7fc8281c124e157eacd00a6d
Matt Hicks <matt@outr.com>
2025-03-28 08:05:17 -0500

    Release 0.11.1

--- scribe/shared/src/main/scala/rapid/RapidLoggerSupport.scala
+++ scribe/shared/src/main/scala/rapid/RapidLoggerSupport.scala
@@ -5,7 +5,8 @@ import scribe.{Level, LogFeature, LogRecord, Logger, LoggerSupport}
 import sourcecode.{FileName, Line, Name, Pkg}
 
 trait RapidLoggerSupport extends Any with LoggerSupport[Task[Unit]] {
-  override def log(record: LogRecord): Task[Unit] = Task(Logger(record.className).log(record))
+  override def log(record: LogRecord): Task[Unit] = Task(Logger(record.className)
+    .log(record.copy(timeStamp = System.currentTimeMillis())))
 
   override def log(level: Level, mdc: MDC, features: LogFeature*)
                   (implicit pkg: Pkg, fileName: FileName, name: Name, line: Line): Task[Unit] =
