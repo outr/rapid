b3428e8ba7c0e94120fc9e080deeaa008012ef69
Matt Hicks <matt@outr.com>
2025-06-20 09:13:25 -0500

    Minor updates to CompletableTask

--- core/shared/src/main/scala/rapid/task/CompletableTask.scala
+++ core/shared/src/main/scala/rapid/task/CompletableTask.scala
@@ -5,23 +5,30 @@ import rapid.Task
 import scala.util.{Failure, Success, Try}
 
 class CompletableTask[Return] extends Task[Return] {
-  @volatile private var result: Option[Try[Return]] = None
+  @volatile private var _result: Option[Try[Return]] = None
+
+  def isComplete: Boolean = _result.nonEmpty
+
+  def result: Option[Try[Return]] = _result
+
+  def isSuccess: Boolean = _result.exists(_.isSuccess)
+  def isFailure: Boolean = _result.exists(_.isFailure)
 
   def success(result: Return): Unit = synchronized {
-    this.result = Some(Success(result))
+    this._result = Some(Success(result))
     notifyAll()
   }
 
   def failure(throwable: Throwable): Unit = synchronized {
-    this.result = Some(Failure(throwable))
+    this._result = Some(Failure(throwable))
     notifyAll()
   }
 
   override def sync(): Return = synchronized {
-    while (result.isEmpty) {
+    while (_result.isEmpty) {
       wait()
     }
-    result.get.get
+    _result.get.get
   }
 
   override def toString: String = "Completable"
